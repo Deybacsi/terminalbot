b7e6d0e7300ada5b4c556153193e42f3d8fadaf9
deybacsi <deyprof@gmail.com>
2020-05-03 13:44:11 +0200

    WSL checker script

---
+++ checkwsl.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+# check to see if it's native linux or running inside windows subsystem for linux
+
+set -e
+if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+    echo "Windows 10 Bash"
+else
+    echo "Anything else"
+fi
